eedd0f2da27f912148c16b2a7f19d43fd798512a
boonbye01 <boonbye01@gmail.com>
2026-02-26 23:49:46 +0700

    Increase compare table qty columns by 10px in actual sale form

--- actual_sale_form.php
+++ actual_sale_form.php
@@ -158,7 +158,7 @@ $remaining_after_deposit = max($total_actual_amount - (float)$customer['coc'], 0
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Ch&#7889;t b&#225;n Sau L&#234;n xe</title>
-  <link rel="stylesheet" href="assets/style.css?v=20260226_mobile20">
+  <link rel="stylesheet" href="assets/style.css?v=20260226_mobile24">
 </head>
 <body>
   <div class="container">
